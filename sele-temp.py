79447d5c3ec92d6f8c31fe85e2021582cf872093
片田 猛士 <katada-t@hi-ad.jp>
2019-05-14 18:05:47 +0900

    指定コード,操作コードの追記

--- sele-temp.py
+++ sele-temp.py
@@ -12,4 +12,34 @@ driver.get('URL')
 
 # 3.操作する要素を指定
 
+# id属性で指定する
+driver.find_element_by_id('ID')
+# class属性で指定する
+driver.find_element_by_class_name('CLASS_NAME')
+# name属性で指定する
+driver.find_element_by_name('NAME')
+# CSSセレクタで指定する
+driver.find_elements_by_css_selector('CSSセレクタ')
+# XPathで指定する
+driver.find_elements_by_xpath('XPath')
+# 完全一致のリンクテキスト
+driver.find_elements_by_link_text('リンクテキスト')
+# 部分一致のリンクテキスト
+driver.find_elements_by_partial_link_text('リンクテキスト')
+
 # 4.その要素を操作する
+
+# クリックする１
+driver.find_element_by_id('ID').click()
+# クリックする２
+# 『.click()』で何故かクリックできないときは、JavaScriptで無理やりクリックさせます。
+element = driver.find_element_by_id('ID')
+driver.execute_script("arguments[0].click();", element)
+# テキストを入力する
+driver.find_element_by_id('ID').send_keys('文字列')
+# テキストを取得する
+driver.find_element_by_id('ID').text
+# 属性値を取得する
+driver.find_element_by_id('ID').get_attribute('属性名')
+# テキストをクリアする
+driver.find_element_by_id('ID').clear()
